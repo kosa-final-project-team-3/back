654ef2776777f1558038a13ca33ca1bbcb1f0344
havegrit <shin_jaean@naver.com>
2024-09-25 15:27:18 +0900

    regenerate project

---
+++ src/main/java/com/kosa/jungdoin/JungdoinApplication.java
@@ -0,0 +1,13 @@
+package com.kosa.jungdoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungdoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JungdoinApplication.class, args);
+    }
+
+}
--- src/test/java/com/kosa/jungdoin/JungdoinApplicationTests.java
+++ src/test/java/com/kosa/jungdoin/JungdoinApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.kosa.jungdoin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AmazingApplicationTests {
+class JungdoinApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
