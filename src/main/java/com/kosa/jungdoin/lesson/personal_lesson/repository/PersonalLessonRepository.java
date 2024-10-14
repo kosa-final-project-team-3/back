af48cb1ed9feef17b02b683024105a9554b2d6f9
havegrit <shin_jaean@naver.com>
2024-10-14 18:17:15 +0900

    feat: add abstract method to get lessons by trainer pk

--- src/main/java/com/kosa/jungdoin/lesson/personal_lesson/repository/PersonalLessonRepository.java
+++ src/main/java/com/kosa/jungdoin/lesson/personal_lesson/repository/PersonalLessonRepository.java
@@ -3,5 +3,8 @@ package com.kosa.jungdoin.lesson.personal_lesson.repository;
 import com.kosa.jungdoin.entity.PersonalLesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonalLessonRepository extends JpaRepository<PersonalLesson, Long> {
+    List<PersonalLesson> findPersonalLessonsByTrainerMemberId(Long trainerId);
 }
