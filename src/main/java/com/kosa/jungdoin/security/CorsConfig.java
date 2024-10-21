899f4c619287615833f74f53b4f7231f449dd01d
havegrit <shin_jaean@naver.com>
2024-10-21 21:59:03 +0900

    feat: add patch method

--- src/main/java/com/kosa/jungdoin/security/CorsConfig.java
+++ src/main/java/com/kosa/jungdoin/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowedOrigins(List.of("http://localhost:5173"));
-		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+		config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
 		config.setAllowedHeaders(List.of("*"));
 		config.setAllowCredentials(true);
 		config.setMaxAge(3600L);
